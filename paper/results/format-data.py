3e3986154d096a476805269cb818f8c8709a7bda
bleaktwig <bleak.twig@gmail.com>
2020-05-26 10:40:36 -0400

    Removed unrelated latex stuff that shouldn't have been there in the first place.

--- paper/results/format-data.py
+++
@@ -1,9 +0,0 @@
-fin = open("processed_data/SET4", 'r', 1)
-file = fin.read()
-fin.close()
-
-counter = 0
-for line in file.splitlines():
-    if counter % 4 == 3:
-        print(line)
-    counter += 1
